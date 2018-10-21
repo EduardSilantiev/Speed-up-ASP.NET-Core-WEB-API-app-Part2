25fc6c39b7679b868e65fba356ea9a236aab5ef4
EduardSilantiev <eduard.silantiev@gmail.com>
2018-10-21 22:06:28 +0300

    initial commit

--- SpeedUpCoreAPIExample/Startup.cs
+++ SpeedUpCoreAPIExample/Startup.cs
@@ -17,7 +17,7 @@ namespace SpeedUpCoreAPIExample
         {
             Configuration = configuration;
         }
-        
+         
         public IConfiguration Configuration { get; }
         
         // This method gets called by the runtime. Use this method to add services to the container.
